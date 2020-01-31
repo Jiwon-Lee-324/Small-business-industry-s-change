40059da8fb28beb0fb46d3416888e275b0a566de
jiwon-lee-324 <jwlee2470@gmail.com>
2020-01-31 00:00:31 +0000

    Self_project

--- small_business_rate.py
+++ small_business_rate.py
@@ -1,6 +1,10 @@
 import csv
 f = open('Properties_Receiving_SBR.csv', 'r', encoding='utf-8')
 rdr = csv.reader(f)
+content = list()
 for line in rdr:
-  print(line)
-f.close()
+  content = line[6],line[5],line[2]
+  print(content)
+  for i in content:
+
+f.close(
